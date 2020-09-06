6eab5ca75cba336ef4c0d17cb59af1f676c7095b
laveryalex <aplavery1@gmail.com>
2020-09-06 23:33:32 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+IndyApp::Application.config.secret_key_base = 'ef3fa12159bd8a357ff0f6731bb4fb4edbdb5ef520794057f3d4ad5f397720474a80a624de39bb2aacb858b01f30aedcf10aba96e1a5d1ba57cb7f78b259efea'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+IndyApp::Application.config.session_store :cookie_store, key: '_indy_app_session'
